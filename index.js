a11741ed75cb04909fb68e4cbe03d95218d27fbb
=mustafa0624 <mustafa.yucel0624@gmail.com>
2020-09-30 01:56:04 +0200

    feat: add focus

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var counter = 0
 let number = Math.ceil(Math.random() * 100);
 
 // console.log(typeof number)
-
+const myInput=document.querySelector("#input");
 document.getElementById("button").addEventListener("click", findNumber)
-document.getElementById("input").addEventListener("keyup", Enter)
+myInput.addEventListener("keyup", Enter)
 
 function findNumber() {
 
@@ -14,6 +14,7 @@ function findNumber() {
     let guess = Number(document.getElementById("input").value)
 
     if (guess === number) {
+        
         // document.getElementById("result").innerHTML = "CONGRATULATIONS !!!"
         alert("CONGRATULATIONS !!!")
         const nextGame = confirm("do you wanna play again?")
@@ -48,7 +49,9 @@ function findNumber() {
 
 
     document.getElementById("test-number").innerHTML = "This is your test number" + " " + counter
-
+    document.querySelector("#input").value = ""
+    myInput.focus();
+    
 }
 
 function Enter(a) {
@@ -62,63 +65,3 @@ function Enter(a) {
 
 
 
-
-// let nr_game;
-// let counter = 0;
-
-// let number = Math.floor((Math.random() * 100) + 1);
-
-// document.getElementById("check_it").addEventListener("click", function () {
-
-//     check_number();
-// });
-
-// document.getElementById("nr_game").addEventListener("keyup", function (event) {
-//     if (event.which === 13) {
-//         check_number();
-
-//     }
-// });
-
-// function check_number() {
-//     counter++;
-//     document.getElementById("count_it").innerHTML = "This is your test number:" + counter;
-
-//     nr_game = document.getElementById("nr_game").value;
-//     if (nr_game == number) {
-
-//         alert("You are the winner :D. " + "You guest after" + (counter--) + " times!");
-
-//         var next_game = confirm("You wanna play again?");
-//         if (next_game == true) {
-//             number = Math.floor((Math.random() * 100) + 1);
-//             counter = 0; document.getElementById("answer").innerHTML = "";
-//             document.getElementById("count_it").innerHTML = "";
-//         }
-//         else {
-//             document.getElementById("count_it").innerHTML = "";
-//             document.getElementById("answer").innerHTML = "Thanks for game!";
-
-//         }
-//     }
-
-//     else if (nr_game == "") {
-//         document.getElementById("answer").innerHTML = "Weite the number";
-//     }
-//     else if (nr_game.indexOf(',') != -1 || nr_game.indexOf('.') != -1) {
-//         document.getElementById("answer").innerHTML = "This is NOT a natural number";
-//     }
-
-//     else if (nr_game > number) {
-//         document.getElementById("answer").innerHTML = "I am thinking about lower number";
-//     }
-//     else if (nr_game < number) {
-//         document.getElementById("answer").innerHTML = "I am thinking about higher number";
-//     }
-//     else if (isNaN(nr_game)) {
-//         document.getElementById("answer").innerHTML = "Are you sure that this is a number?";
-//     }
-
-//     document.getElementById("nr_game").value = '';
-//     document.getElementById("nr_game").focus();
-// }
